7865cf9cfdfbb9a2f34481676597e5217a19cfc1
Emil Bülow <emilbulow@outlook.com>
2013-11-09 21:33:35 +0100

    Browser Sample

---
+++ UbpSample_Browser/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using UbpSample_Browser.Resources;
+
+namespace UbpSample_Browser
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ UbpSample_Browser/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using UbpSample_Browser.Resources;
+
+namespace UbpSample_Browser
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.IsScriptEnabled = true;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //[UBP]
+            Uri uri = App.RootFrame.UriMapper.MapUri(e.Uri);
+            ParseNavigationUri(uri);
+
+            base.OnNavigatedTo(e);
+        }
+
+        //[UBP]
+        public void ParseNavigationUri(Uri uri)
+        {
+            string uriString = uri.ToString();
+
+            //Get key/value pairs from query
+            string[] args = uriString.Substring(uriString.IndexOf("?") + 1).Split(new string[] { "&" }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Cycle through key/value pair and navigate to url
+            foreach (string arg in args)
+            {
+                if (arg.Contains("="))
+                {
+                    //var[0]=key, var[1]=value
+                    string[] var = arg.Split(new string[] { "=" }, StringSplitOptions.None);
+                    switch (var[0])
+                    {
+                        case "url":
+                            {
+                                Browser.Navigate(new Uri(var[1], UriKind.Absolute));
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
